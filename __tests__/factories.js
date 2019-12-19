75518f7c0c7729b2037a01188954dccdc8fc3544
Stefano Saffran <stefanosaffran@hotmail.com>
2019-12-19 12:05:24 +0100

    feat: add tests helper functions

--- __tests__/factories.js
+++ __tests__/factories.js
@@ -1,12 +1,18 @@
 import faker from 'faker';
 import { factory } from 'factory-girl';
+import { startOfHour, addHours } from 'date-fns';
 
 import User from '../src/app/models/User';
+import Appointment from '../src/app/models/Appointment';
 
-factory.define('User', User, {
+factory.define('User', User, () => ({
   name: faker.name.findName(),
   email: faker.internet.email(),
   password: faker.internet.password(),
-});
+}));
+
+factory.define('Appointment', Appointment, () => ({
+  date: startOfHour(addHours(new Date(), 1)),
+}));
 
 export default factory;
--- __tests__/getToken.js
+++ __tests__/getToken.js
@@ -3,8 +3,11 @@ import request from 'supertest';
 import app from '../src/app';
 import factory from './factories';
 
-const getToken = async () => {
-  const user = await factory.attrs('User');
+export const getTokenProvider = async () => {
+  const user = await factory.attrs('User', {
+    provider: true,
+    id: 1,
+  });
 
   await request(app)
     .post('/users')
@@ -17,4 +20,18 @@ const getToken = async () => {
   return body.token;
 };
 
-export default getToken;
+export const getToken = async () => {
+  const user = await factory.attrs('User', {
+    id: 1,
+  });
+
+  await request(app)
+    .post('/users')
+    .send(user);
+
+  const { body } = await request(app)
+    .post('/sessions')
+    .send(user);
+
+  return body.token;
+};
