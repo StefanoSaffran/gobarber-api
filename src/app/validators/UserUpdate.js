12e287b5e1a2282997ae0a437929f50b0d0549e5
Stefano Saffran <stefanosaffran@hotmail.com>
2020-01-04 10:58:36 +0100

    chore: moving yup validation from controllers

---
+++ src/app/validators/UserUpdate.js
@@ -0,0 +1,31 @@
+import * as Yup from 'yup';
+
+export default async (req, res, next) => {
+  try {
+    /**
+     * Check if the user sent the oldPassword, if so, make the password and the
+     * confirmPassword required, if not, make them optional.
+     */
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    await schema.validate(req.body, { abortEarly: false });
+
+    return next();
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ error: 'validation fails', messages: error.inner });
+  }
+};
